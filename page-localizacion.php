88616d2faaef7dc9846cd06c974629a9ad0cb816
molinerozadkiel <molinerozadkiel@gmail.com>
2019-09-16 17:07:54 -0300

    parapapa

---
+++ page-localizacion.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+
+
+<section class="destinosContainer section">
+
+  <a href="#" target="_blank">
+    <aside class="ibizaAndorra andorra">
+      <div class="ibizaAndorraTxt andorraText">
+        <h2 class="title">Andorra</h2>
+        <p>El encanto del día de playa ibicenco en la alta montaña andorrana, en Pas de la Casa. Un día de esquí puede acabar con una sangría bien fresca y una paella. </p>
+      </div>
+      <img class="andorraImg" src="<?php echo get_template_directory_uri(); ?>/img/destinos/andorra.jpg" alt="Mujer-sonriendo-con-los-pirineos-nevados-den-el-fondo">
+    </aside>
+  </a>
+
+  <a href="#" target="_blank">
+    <aside class="ibizaAndorra ibiza">
+      <div class="ibizaAndorraTxt ibizaText">
+        <h2 class="title">Ibiza</h2>
+        <p>Al oeste de Ibiza, a orillas del Mediterráneo, la sabia Diosa Tanit guarda un idílico rincón de sol, fina arena blanca y aguas turquesa. No es otra playa paradisíaca cualquiera: es la que recordarás para siempre</p>
+      </div>
+      <img class="ibizaImg" src="<?php echo get_template_directory_uri(); ?>/img/destinos/ibiza.jpg" alt="grupo-de-jovenes-con-mediterraneo-de-fondo">
+    </aside>
+  </a>
+
+  <!-- <img class="logoIbizaAndorra" src="<?php echo get_template_directory_uri(); ?>/img/destinos/logo-blanco.png" alt="Logo-CBbC-en-blanco"> -->
+
+</section>
+
+
+<?php get_footer(); ?>
